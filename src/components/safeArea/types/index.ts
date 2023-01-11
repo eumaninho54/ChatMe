87a281e78e965b64c7c80142565ccaf38a62a528
eumaninho54 <angelo.omarzinho@outlook.com>
2023-01-11 00:12:00 -0300

    components - SafeArea finished

---
+++ src/components/safeArea/types/index.ts
@@ -0,0 +1,7 @@
+import { Edge } from "react-native-safe-area-context"
+import { IColor } from "../../../styles/colors/types"
+
+export type IProps = {
+  edges?: readonly Edge[]
+  color?: IColor
+}
