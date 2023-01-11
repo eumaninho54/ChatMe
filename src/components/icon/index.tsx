2b0452defc846d57f88427b1874dc53f85f72233
eumaninho54 <angelo.omarzinho@outlook.com>
2023-01-11 00:09:56 -0300

    components - prop icon family

--- src/components/icon/index.tsx
+++ src/components/icon/index.tsx
@@ -1,5 +1,6 @@
 import React, { forwardRef, memo, useContext } from 'react';
 import IconIonicons from 'react-native-vector-icons/Ionicons';
+import IconFontAwesome5 from 'react-native-vector-icons/FontAwesome5'
 import { ThemeContext } from 'styled-components';
 import { IColor } from '../../styles/colors/types';
 import { buildIcon } from './library';
@@ -9,13 +10,31 @@ const Icon = forwardRef<IconIonicons, IProps>((props, ref) => {
   const theme = useContext<Record<IColor, string>>(ThemeContext)
   const icon = buildIcon(theme, props)
 
+
   return (
-    <IconIonicons 
-      ref={ref}
-      key={props.key}
-      name={icon.name} 
-      size={icon.size}
-      color={icon.color}/>
+    <>
+      {
+        props.family == 'FontAwesome5'
+          ? <IconFontAwesome5
+            ref={ref}
+            key={props.key}
+            name={icon.name}
+            size={icon.size}
+            color={icon.color} />
+          : null
+      }
+
+      {
+        props.family == 'Ionicons'
+          ? <IconIonicons
+            ref={ref}
+            key={props.key}
+            name={icon.name}
+            size={icon.size}
+            color={icon.color} />
+          : null
+      }
+    </>
   )
 })
 
--- src/components/icon/types/index.ts
+++ src/components/icon/types/index.ts
@@ -2,9 +2,12 @@ import { IColor } from '../../../styles/colors/types';
 
 export type IIconSizeKey = 'extra_small_10' | 'small_15' | 'normal_20' | 'big_25' | 'extra_big_30'
 
+export type IIconFamilyKey = 'FontAwesome5' | 'Ionicons'
+
 export type IProps = {
   name: string
   size: IIconSizeKey
   color: IColor
+  family: IIconFamilyKey
   key?: string
 }
