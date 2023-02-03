db435004a47a1e8adb1e50f2b24134cf182d136c
eumaninho54 <angelo.omarzinho@outlook.com>
2023-02-02 22:40:46 -0300

    routes - tab bar float button

--- src/routes/tab/index.tsx
+++ src/routes/tab/index.tsx
@@ -1,13 +1,14 @@
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { BottomTabBar, createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { useNavigation } from '@react-navigation/native';
 import React, { useContext } from 'react';
-import { View } from 'react-native';
+import { TouchableOpacity, View } from 'react-native';
+import { G, Path } from 'react-native-svg';
 import { ThemeContext } from 'styled-components/native';
 import Icon from '../../components/icon';
 import Add from '../../pages/add';
 import Home from '../../pages/home';
 import { ITheme } from '../../styles/colors/types';
-import { AddButton, AddIcon, IconWrapper, SafeAreaView } from './styles';
+import { AddButton, AddIcon, AddWrapper, IconWrapper, SafeAreaView, Svg, Wrapper } from './styles';
 import { TabParamList, CustomTabBarButtonProps, TabScreenNavigationProps } from './types';
 
 const { Navigator, Screen } = createBottomTabNavigator<TabParamList>()
@@ -18,79 +19,96 @@ export const TabNav: React.FC = () => {
 
   const CustomTabBarButton = (props: CustomTabBarButtonProps) => {
     return (
-      <AddButton onPress={() => navigate('Add', { screen: 'tab' })}>
-        <AddIcon>
-          {props.children}
-        </AddIcon>
-      </AddButton>
+      <AddWrapper>
+        <Svg
+          width={97.5} height={79.3} viewBox="0 0 97.5 79.3"
+          color={theme.secundaryBackground}>
+          <G transform={'scale(1.3)'}>
+            <Path
+              d="M75.2 0v61H0V0
+                c4.1 0 7.4 3.1 7.9 7.1C10 21.7 22.5 33 37.7 33
+                c15.2 0 27.7-11.3 29.7-25.9.5-4 3.9-7.1 7.9-7.1
+                h-.1z"
+              fill={theme.secundaryBackground} />
+          </G>
+        </Svg>
+
+        <AddButton onPress={() => navigate('Add', { screen: 'tab' })}>
+          <AddIcon>{props.children}</AddIcon>
+        </AddButton>
+      </AddWrapper>
     )
   }
 
   return (
     <SafeAreaView
       edges={['bottom']}>
-      <Navigator
-        initialRouteName='Home'
-        sceneContainerStyle={{ backgroundColor: theme.primaryBackground }}
-        screenOptions={{
-          headerShadowVisible: false,
-          headerShown: false,
-          tabBarShowLabel: false,
-          tabBarStyle: {
-            backgroundColor: theme.secundaryBackground,
-            height: 70,
-            paddingBottom: 0,
-            borderTopWidth: 0
-          }
-        }}>
-        <Screen
-          name='Home'
-          component={Home}
-          options={{
-            tabBarIcon: ({ focused }) => (
-              <View>
-                <Icon
-                  family='Ionicons'
-                  name={focused ? 'chatbubbles' : 'chatbubbles-outline'}
-                  size={'extra_big_30'}
-                  color={focused ? 'primaryColor' : 'secundaryFont'} />
-              </View>
-            )
-          }} />
+      <Wrapper>
+        <Navigator
+          initialRouteName='Home'
+          sceneContainerStyle={{ backgroundColor: theme.secundaryBackground }}
+          screenOptions={{
+            headerShadowVisible: false,
+            headerShown: false,
+            tabBarShowLabel: false,
+            tabBarStyle: {
+              backgroundColor: theme.secundaryBackground,
+              height: 70,
+              paddingBottom: 0,
+              borderTopWidth: 0,
+              borderTopLeftRadius: 20,
+              borderTopRightRadius: 20,
+            }
+          }}>
+          <Screen
+            name='Home'
+            component={Home}
+            options={{
+              tabBarIcon: ({ focused }) => (
+                <View>
+                  <Icon
+                    family='Ionicons'
+                    name={focused ? 'chatbubbles' : 'chatbubbles-outline'}
+                    size={'extra_big_30'}
+                    color={focused ? 'primaryColor' : 'secundaryFont'} />
+                </View>
+              )
+            }} />
 
-        <Screen
-          name='Add'
-          component={Add}
-          options={{
-            tabBarIcon: () => (
-              <IconWrapper>
-                <Icon
-                  family='Ionicons'
-                  name={'add'}
-                  size={'extra_big_30'}
-                  color={'white'} />
-              </IconWrapper>
-            ),
-            tabBarButton: (props: any) => (
-              <CustomTabBarButton {...props}/>
-            )
-          }} />
+          <Screen
+            name='Add'
+            component={Add}
+            options={{
+              tabBarIcon: () => (
+                <IconWrapper>
+                  <Icon
+                    family='Ionicons'
+                    name={'add'}
+                    size={'giant_35'}
+                    color={'white'} />
+                </IconWrapper>
+              ),
+              tabBarButton: (props) => (
+                <CustomTabBarButton {...props} />
+              )
+            }} />
 
-        <Screen
-          name='Settings'
-          component={Home} // Test
-          options={{
-            tabBarIcon: ({ focused }) => (
-              <View>
-                <Icon
-                  family='Ionicons'
-                  name={focused ? 'settings' : 'settings-outline'}
-                  size={'extra_big_30'}
-                  color={focused ? 'primaryColor' : 'secundaryFont'} />
-              </View>
-            )
-          }} />
-      </Navigator>
+          <Screen
+            name='Settings'
+            component={Home} // Test
+            options={{
+              tabBarIcon: ({ focused }) => (
+                <View>
+                  <Icon
+                    family='Ionicons'
+                    name={focused ? 'settings' : 'settings-outline'}
+                    size={'extra_big_30'}
+                    color={focused ? 'primaryColor' : 'secundaryFont'} />
+                </View>
+              )
+            }} />
+        </Navigator>
+      </Wrapper>
     </SafeAreaView>
   )
 }
--- src/routes/tab/styles.ts
+++ src/routes/tab/styles.ts
@@ -1,27 +1,41 @@
 import { SafeAreaView as safeAreaView } from "react-native-safe-area-context";
+import { Svg as svg } from "react-native-svg";
 import styled from "styled-components/native";
 import { ITheme } from "../../styles/colors/types";
 
-type SafeAreaViewProps = {
+type Theme = {
   theme: ITheme
 }
 
 export const SafeAreaView = styled(safeAreaView)`
   flex: 1; 
-  background-color: ${({theme}: SafeAreaViewProps) => theme.secundaryBackground};
+  background-color: ${({theme}: Theme) => theme.secundaryBackground};
+`
+
+export const Wrapper = styled.View`
+  flex: 1; 
+  background-color: ${({theme}: Theme) => theme.primaryBackground};
+`
+
+export const AddWrapper = styled.View`
+  position: relative;
+  width: 75px;
+  align-items: center;
+`
+
+export const Svg = styled(svg)`
+  position: absolute;
+  top: 0;
+  background-color: ${({theme}: Theme) => theme.primaryBackground};
 `
 
 export const AddButton = styled.TouchableOpacity`
-  top: -30px;
+  top: -25px;
   justify-content: center;
   align-items: center;
 `
 
-type AddProps = {
-  theme: ITheme
-}
-
-export const AddIcon = styled.View<AddProps>`
+export const AddIcon = styled.View<Theme>`
   width: 60px;
   height: 60px;
   border-radius: ${`${60 / 2}px`};
