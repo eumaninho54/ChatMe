7a74eb4f176fd94686e0ef371ec5482e0e826257
eumaninho54 <angelo.omarzinho@outlook.com>
2023-02-05 22:40:42 -0300

    routes - tab float button add

--- src/routes/tab/index.tsx
+++ src/routes/tab/index.tsx
@@ -1,14 +1,14 @@
 import { BottomTabBar, createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { useNavigation } from '@react-navigation/native';
-import React, { useContext } from 'react';
-import { TouchableOpacity, View } from 'react-native';
+import React, { useContext, useRef, useState } from 'react';
+import { Animated, Dimensions, TouchableOpacity, View } from 'react-native';
 import { G, Path } from 'react-native-svg';
 import { ThemeContext } from 'styled-components/native';
 import Icon from '../../components/icon';
 import Add from '../../pages/add';
 import Home from '../../pages/home';
 import { ITheme } from '../../styles/colors/types';
-import { AddButton, AddIcon, AddWrapper, IconWrapper, SafeAreaView, Svg, Wrapper } from './styles';
+import { MenuIcon, MenuButton, AddWrapper, IconWrapper, SafeAreaView, Svg, Wrapper, ItemIcon, ItemsWrapper } from './styles';
 import { TabParamList, CustomTabBarButtonProps, TabScreenNavigationProps } from './types';
 
 const { Navigator, Screen } = createBottomTabNavigator<TabParamList>()
@@ -16,27 +16,86 @@ const { Navigator, Screen } = createBottomTabNavigator<TabParamList>()
 export const TabNav: React.FC = () => {
   const { navigate } = useNavigation<TabScreenNavigationProps>()
   const theme = useContext<ITheme>(ThemeContext)
+  const animation = useRef(new Animated.Value(0)).current
+  const [isOpen, setIsOpen] = useState(false)
+
+
+  const bottom = animation.interpolate({
+    inputRange: [0, 1],
+    outputRange: [0, 115]
+  })
+
+  const rotation = animation.interpolate({
+    inputRange: [0, 1],
+    outputRange: ['0deg', '45deg']
+  })
+
+  const onToggleMenu = () => {
+    const toValue = isOpen ? 0 : 1
+
+    Animated.spring(animation, {
+      toValue,
+      friction: 5,
+      useNativeDriver: false
+    }).start()
+
+    setIsOpen(!isOpen)
+  }
 
   const CustomTabBarButton = (props: CustomTabBarButtonProps) => {
     return (
-      <AddWrapper>
-        <Svg
-          width={97.5} height={79.3} viewBox="0 0 97.5 79.3"
-          color={theme.secundaryBackground}>
-          <G transform={'scale(1.3)'}>
-            <Path
-              d="M75.2 0v61H0V0
+      <>
+        <AddWrapper>
+          <Svg
+            width={97.5} height={79.3} viewBox="0 0 97.5 79.3"
+            color={theme.secundaryBackground}>
+            <G transform={'scale(1.3)'}>
+              <Path
+                d="M75.2 0v61H0V0
                 c4.1 0 7.4 3.1 7.9 7.1C10 21.7 22.5 33 37.7 33
                 c15.2 0 27.7-11.3 29.7-25.9.5-4 3.9-7.1 7.9-7.1
                 h-.1z"
-              fill={theme.secundaryBackground} />
-          </G>
-        </Svg>
+                fill={theme.secundaryBackground} />
+            </G>
+          </Svg>
 
-        <AddButton onPress={() => navigate('Add', { screen: 'tab' })}>
-          <AddIcon>{props.children}</AddIcon>
-        </AddButton>
-      </AddWrapper>
+          <MenuButton
+            activeOpacity={1}
+            onPress={onToggleMenu}>
+            <MenuIcon
+              style={{ transform: [{ rotate: rotation }] }}>
+              {props.children}
+            </MenuIcon>
+          </MenuButton>
+        </AddWrapper>
+
+        <ItemsWrapper
+          style={{
+            bottom: bottom,
+            transform: [
+              { scale: animation }
+            ]
+          }}
+          pointerEvents='box-none'>
+          <ItemIcon style={{elevation: 4}}>
+            <Icon
+              solid
+              family='FontAwesome5'
+              name='user'
+              color='primaryColor'
+              size='normal_20' />
+          </ItemIcon>
+
+          <ItemIcon style={{elevation: 4}}>
+            <Icon
+              solid
+              family='FontAwesome5'
+              name='users'
+              color='primaryColor'
+              size='normal_20' />
+          </ItemIcon>
+        </ItemsWrapper>
+      </>
     )
   }
 
--- src/routes/tab/styles.ts
+++ src/routes/tab/styles.ts
@@ -1,49 +1,73 @@
-import { SafeAreaView as safeAreaView } from "react-native-safe-area-context";
-import { Svg as svg } from "react-native-svg";
-import styled from "styled-components/native";
-import { ITheme } from "../../styles/colors/types";
+import { Animated, View } from 'react-native';
+import {SafeAreaView as safeAreaView} from 'react-native-safe-area-context';
+import {Svg as svg} from 'react-native-svg';
+import styled from 'styled-components/native';
+import {ITheme} from '../../styles/colors/types';
 
 type Theme = {
-  theme: ITheme
-}
+  theme: ITheme;
+};
 
 export const SafeAreaView = styled(safeAreaView)`
-  flex: 1; 
+  flex: 1;
   background-color: ${({theme}: Theme) => theme.secundaryBackground};
-`
+`;
 
 export const Wrapper = styled.View`
-  flex: 1; 
+  flex: 1;
   background-color: ${({theme}: Theme) => theme.primaryBackground};
-`
+`;
 
 export const AddWrapper = styled.View`
   position: relative;
   width: 75px;
   align-items: center;
-`
+  z-index: 3;
+`;
 
 export const Svg = styled(svg)`
   position: absolute;
   top: 0;
   background-color: ${({theme}: Theme) => theme.primaryBackground};
-`
+`;
 
-export const AddButton = styled.TouchableOpacity`
+export const MenuButton = styled.TouchableOpacity`
   top: -25px;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const MenuIcon = styled(Animated.View)`
+  width: 62px;
+  height: 62px;
+  border-radius: ${`${62 / 2}px`};
+  background-color: ${({theme}: Theme) => theme.primaryColor};
+  border-width: 3px;
+  border-color: ${({theme}: Theme) => theme.primaryBackground};
+  justify-content: center;
+  align-items: center;
+`;
+
+export const ItemsWrapper = styled(Animated.View)`
+  width: 100%;
+  position: absolute;
   justify-content: center;
   align-items: center;
 `
 
-export const AddIcon = styled.View<Theme>`
-  width: 60px;
-  height: 60px;
-  border-radius: ${`${60 / 2}px`};
-  background-color: ${({theme}) => theme.primaryColor};
-`
+export const ItemIcon = styled.TouchableOpacity<Theme>`
+  width: 50px;
+  height: 50px;
+  border-radius: ${`${50 / 2}px`};
+  background-color: ${({theme}) => theme.white};
+  justify-content: center;
+  align-items: center;
+  box-shadow: 1px 1px 5px ${({theme}) => theme.secundaryColor};
+  margin-top: 15px;
+`;
 
 export const IconWrapper = styled.View`
   justify-content: center;
   align-items: center;
   margin-left: 1px;
-`
+`;
