7805672e19dadbbe7da85adbf879a79ea36eafef
eumaninho54 <angelo.omarzinho@outlook.com>
2023-02-07 23:56:06 -0300

    store - exitUserAction

--- src/store/user/index.ts
+++ src/store/user/index.ts
@@ -1,13 +1,18 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { exitUserAction } from "./actions/exitUser";
 import { initialState } from "./initialState";
 import { userAsyncBuilder } from './thunks';
 
 const userSlice = createSlice({
   name: "user",
   initialState: initialState,
-  reducers: { },
+  reducers: { 
+    exitUser(){
+      return exitUserAction()
+    }
+  },
   extraReducers: (builder) => userAsyncBuilder(builder)
 })
 
 export default userSlice.reducer;
-export const { } = userSlice.actions;
+export const { exitUser } = userSlice.actions;
