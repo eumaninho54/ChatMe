370d56c24c4fbc2b74ebbe4a66d814853bef6471
eumaninho54 <angelo.omarzinho@outlook.com>
2023-02-13 00:26:08 -0300

    pages - messages contact in home

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,16 +1,131 @@
-import React, { useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import Header from './components/header';
-import { SafeAreaView, Wrapper } from './styles';
+import { AvatarWrapper, CheckWrapper, FooterInfoWrapper, HeaderInfoWrapper, InfoWrapper, ItemSeparator, LinearGradient, LinearGradientWrapper, MaskedView, MessageWrapper, NotReadWrapper, RenderItemWrapper, SafeAreaView, UsernameWrapper, Wrapper } from './styles';
 import '../../services/websockets';
 import Empty from '../../components/empty';
 import DownArrow from '../../assets/arrow/arrow.png'
 import { useTranslation } from 'react-i18next';
+import { useAppDispatch, useAppSelector } from '../../store/hooks';
+import { reactotron } from '../../config/reactotron';
+import { apiError } from '../../errors/apiError';
+import { exitUser } from '../../store/reducers/user';
+import { getAllThunk } from '../../store/reducers/messages/thunks/getAllThunk';
+import { FlatList } from 'react-native';
+import { IMessages } from '../../store/reducers/messages/types';
+import Text from '../../components/text';
+import Avatar from '../../components/avatar';
+import { ThemeContext } from 'styled-components/native';
+import { ITheme } from '../../styles/colors/types';
+import Icon from '../../components/icon';
 
 
 const Home: React.FC = () => {
   const { t, i18n } = useTranslation()
+  const user = useAppSelector((store) => store.user)
+  const theme = useContext<ITheme>(ThemeContext)
   const [searchValue, setSearchValue] = useState('')
-  const mockadoData = []
+  const messages = useAppSelector((store) => store.messages)
+  const dispatch = useAppDispatch()
+
+  const callbackApiError = () => {
+    dispatch(exitUser())
+  }
+
+  const renderItem = ({ item, index }: { item: IMessages, index: number }) => {
+
+    const check = () => {
+      if (item.messages?.[0]?.senderId == user.id) {
+        if (item.messages?.[0].isReceived && item.messages?.[0].isRead)
+          return (
+            <Icon
+              family='Ionicons'
+              color='focused'
+              name='checkmark-done'
+              size='small_15' />
+          )
+
+        else if (item.messages?.[0].isReceived)
+          return (
+            <Icon
+              family='Ionicons'
+              color='secundaryFont'
+              name='checkmark-done'
+              size='small_15' />
+          )
+
+        else return (
+          <Icon
+            family='Ionicons'
+            color='secundaryFont'
+            name='checkmark'
+            size='small_15' />
+        )
+      }
+
+      return ''
+    }
+
+    return (
+      <RenderItemWrapper>
+        <AvatarWrapper>
+          <Avatar source={{ uri: item.avatarFriend }} />
+        </AvatarWrapper>
+
+        <InfoWrapper>
+          <HeaderInfoWrapper>
+            <UsernameWrapper>
+              <Text
+                text={item.usernameFriend.split('#')[0]}
+                color='primaryFont'
+                size='normal_16'
+                weight='semibold'
+                numberOfLines={1} />
+            </UsernameWrapper>
+
+            <Text
+              text={'08:34 PM'}
+              color='secundaryFont'
+              size='normal_16'
+              weight='regular' />
+          </HeaderInfoWrapper>
+
+          <FooterInfoWrapper>
+            <MessageWrapper>
+              <CheckWrapper>
+                {check()}
+              </CheckWrapper>
+
+              <Text
+                text={item?.messages[0]?.message}
+                color='secundaryFont'
+                size='normal_16'
+                weight='regular'
+                numberOfLines={2} />
+            </MessageWrapper>
+
+            {item.notRead
+              ?
+              <NotReadWrapper>
+                <Text
+                  text={item.notRead >= 20 ? `${item.notRead}+` : String(item.notRead)}
+                  color='white'
+                  size='small_14'
+                  weight='semibold' />
+              </NotReadWrapper>
+              : null
+            }
+          </FooterInfoWrapper>
+        </InfoWrapper>
+      </RenderItemWrapper>
+    )
+  }
+
+  useEffect(() => {
+    dispatch(getAllThunk({ idUser: user.id }))
+      .unwrap()
+      .catch((err) => apiError({ err, t, callbackApiError }))
+  }, [])
+
 
   return (
     <SafeAreaView edges={['top']}>
@@ -18,7 +133,7 @@ const Home: React.FC = () => {
         searchValue={searchValue}
         setSearchValue={setSearchValue} />
 
-      {mockadoData.length == 0
+      {messages.length == 0
         ?
         <Empty
           title={t('No chat yet...')}
@@ -26,12 +141,27 @@ const Home: React.FC = () => {
           srcImage={DownArrow} />
 
         :
-        <Wrapper>
-
-        </Wrapper>
-
+        <MaskedView
+          maskElement={
+            <LinearGradientWrapper>
+              <LinearGradient
+                locations={[0, 0.05, 0.95, 1]}
+                colors={[
+                  '#00000000',
+                  theme.secundaryBackground,
+                  theme.secundaryBackground,
+                  '#00000000'
+                ]} />
+            </LinearGradientWrapper>
+          }>
+          <FlatList
+            data={messages}
+            renderItem={renderItem}
+            ItemSeparatorComponent={() => <ItemSeparator />}
+            contentContainerStyle={{ paddingVertical: 20 }}
+            showsVerticalScrollIndicator={false} />
+        </MaskedView>
       }
-
     </SafeAreaView>
   )
 }
--- src/pages/home/styles.ts
+++ src/pages/home/styles.ts
@@ -1,18 +1,93 @@
 import {SafeAreaView as safeAreaView} from 'react-native-safe-area-context';
 import styled from 'styled-components/native';
 import {ITheme} from '../../styles/colors/types';
+import Maskedview from '@react-native-community/masked-view';
+import Lineargradient from 'react-native-linear-gradient';
 
-type SafeAreaViewProps = {
+type IProps = {
   theme: ITheme;
 };
 
 export const SafeAreaView = styled(safeAreaView)`
-  background-color: ${({theme}: SafeAreaViewProps) => theme.primaryBackground};
+  background-color: ${({theme}: IProps) => theme.primaryBackground};
   flex: 1;
   padding: 0px 15px;
 `;
 
-export const Wrapper = styled.ScrollView`
+export const Wrapper = styled.View`
   flex: 1;
-  margin-top: 10px;
+`
+
+export const MaskedView = styled(Maskedview)`
+  height: 100%;
+  flex: 1;
+  flex-direction: row;
+`
+
+export const LinearGradientWrapper = styled.View`
+  background-color: transparent; 
+  flex: 1;
+`
+
+export const LinearGradient = styled(Lineargradient)`
+  flex: 1; 
+  width: 100%; 
+  border-radius: 10;
+`
+
+export const ItemSeparator = styled.View`
+  height: 1px;
+  margin: 10px 0px;
+`
+
+export const RenderItemWrapper = styled.TouchableOpacity<IProps>`
+  background-color: ${({theme}) => theme.secundaryBackground};
+  border-radius: 10px;
+  flex-direction: row;
+  padding: 10px;
+`
+
+export const AvatarWrapper = styled.View`
+  margin-right: 10px;
+`
+
+export const InfoWrapper = styled.View`
+  flex: 1; 
+  justify-content: center;
+`
+
+export const HeaderInfoWrapper = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  margin-bottom: 10px;
+`
+
+export const UsernameWrapper = styled.View`
+  flex: 1;
+`
+
+export const FooterInfoWrapper = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+`
+
+export const MessageWrapper = styled.View`
+  flex: 1;
+  flex-direction: row;
+  align-items: center;
+`
+
+export const CheckWrapper = styled.View`
+  margin-right: 2px;
+`
+
+export const NotReadWrapper = styled.View<IProps>`
+  background-color: ${({theme}) => theme.primaryColor};
+  justify-content: center;
+  align-items: center;
+  border-radius: 99;
+  height: 25;
+  min-width: 25;
+  padding: 0px 5px;
+  margin-left: 15;
 `
