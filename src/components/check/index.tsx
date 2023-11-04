3ca76c4fe3671324e75a89956c77b5d9e8a98730
eumaninho54 <angelo.omarzinho@outlook.com>
2023-11-04 06:51:12 -0300

    feat - check component

--- src/components/check/index.tsx
+++ src/components/check/index.tsx
@@ -2,15 +2,21 @@ import React from 'react';
 import { useAppSelector } from '../../store/hooks';
 import Icon from '../icon';
 import { IProps } from './types';
+import { reactotron } from '../../config/reactotron';
 
 
 const Check: React.FC<IProps> = (props) => {
-  const { message } = props
+  const { message, users } = props
   const user = useAppSelector((store) => store.user)
 
   const check = () => {
-    if (message.senderId == user.id) {
-      if (message?.isReceived && message?.isRead)
+    const isSender = message.sender.id == user.id
+    const hasReceived = users.every((item) => message.isReceivedBy.includes(item.id))
+
+    const hasRead = users.every((item) => message.isReadBy.includes(item.id))
+
+    if (isSender) {
+      if (hasReceived && hasRead)
         return (
           <Icon
             family='Ionicons'
@@ -19,7 +25,7 @@ const Check: React.FC<IProps> = (props) => {
             size='normal_20' />
         )
 
-      else if (message.isReceived)
+      else if (hasReceived)
         return (
           <Icon
             family='Ionicons'
--- src/components/check/types/index.ts
+++ src/components/check/types/index.ts
@@ -1,5 +1,7 @@
+import { IUser } from '../../../store/reducers/user/types';
 import { IMessages } from './../../../store/reducers/messages/types/index';
 
 export type IProps = {
   message: IMessages
+  users: Omit<IUser, "auth" | "accessToken" | "refreshToken">[]
 }
