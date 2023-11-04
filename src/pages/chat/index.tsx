2c28b812907f123fd3648e8aad437df9bc5a8e70
eumaninho54 <angelo.omarzinho@outlook.com>
2023-11-04 06:54:30 -0300

    feat - chat page

--- src/pages/chat/index.tsx
+++ src/pages/chat/index.tsx
@@ -1,45 +1,68 @@
 import { useNavigation, useRoute } from '@react-navigation/native';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { FlatList, View } from 'react-native';
 import Check from '../../components/check';
 import Text from '../../components/text';
 import { IChatRouteType, IStackNavigation } from '../../routes/types';
-import { useAppSelector } from '../../store/hooks';
-import { IChat, IMessages } from '../../store/reducers/messages/types';
+import { useAppDispatch, useAppSelector } from '../../store/hooks';
+import { IMessages } from '../../store/reducers/messages/types';
 import { formatDate } from '../../utils/formatDate';
 import ChatInput from './components/chatInput';
 import Header from './components/header';
 import Speech from './components/speech';
-import { DateMessageWrapper, MessagesWrapper, OuterMessage, SafeAreaView, StatusMessage, Wrapper } from './styles';
+import {
+  DateMessageWrapper,
+  MessagesWrapper,
+  OuterMessage,
+  SafeAreaView,
+  StatusMessage,
+  Wrapper
+} from './styles';
+import { useWebSocket } from '../../hooks/useWebSocket/useWebSocket';
+import { readChat } from '../../store/reducers/messages';
 
 const Chat: React.FC = () => {
-  const { params: { messagesChat } } = useRoute<IChatRouteType>()
+  const { params: { chat } } = useRoute<IChatRouteType>()
   const { goBack } = useNavigation<IStackNavigation>()
+  const { onReadMessage } = useWebSocket()
   const user = useAppSelector((store) => store.user)
-  const messages = useAppSelector((store) => store.messages)
-  const [chat, setChat] = useState(messagesChat)
+  const allMessages = useAppSelector((store) => store.messages)
+  const dispatch = useAppDispatch()
+  const messages = allMessages.find((item) => item.idChat == chat.idChat)?.messages 
+
+  useEffect(() => {
+    handleReadMessage()
+    dispatch(readChat({ idChat: chat.idChat }))
+  }, [messages])
 
 
   useEffect(() => {
-    const newChat: IChat = {
-      ...messages.find((message) => message.idChat == chat.idChat) as IChat,
-      icon: messagesChat.icon,
-      name: messagesChat.name
-    }
+    handleReadMessage()
+    dispatch(readChat({ idChat: chat.idChat }))
+  }, [])
 
-    newChat 
-      ? setChat(newChat)
-      : goBack()
-  }, [messages])
+  const handleReadMessage = () => {
+    const messagesToRead = messages?.filter((item) => {
+      const isReadBy = item.isReadBy.includes(user.id)
+      return item.sender.id != user.id && !isReadBy
+    })
+
+    if (messagesToRead && messagesToRead.length > 0) {
+      onReadMessage({
+        idChat: chat.idChat,
+        messagesId: messagesToRead.map((item) => item.id)
+      })
+    }
+  }
 
   const renderItem = ({ item, index }: { item: IMessages, index: number }) => {
     return (
       <OuterMessage
-        isOwner={user.id == item.senderId}>
+        isOwner={user.id == item.sender.id}>
         <View>
           <Speech
             message={item.message}
-            isOwner={user.id == item.senderId} />
+            isOwner={user.id == item.sender.id} />
 
           <StatusMessage>
             <DateMessageWrapper>
@@ -51,7 +74,7 @@ const Chat: React.FC = () => {
               />
             </DateMessageWrapper>
 
-            <Check message={item} />
+            <Check message={item} users={chat.users} />
           </StatusMessage>
         </View>
       </OuterMessage>
@@ -64,18 +87,19 @@ const Chat: React.FC = () => {
         <Header
           id={chat.idChat}
           avatar={chat.icon || ''}
-          username={chat.name || ''}
+          username={chat.name}
           isOnline={chat.isOnline} />
 
         <MessagesWrapper>
           <FlatList
-            data={chat.messages}
-            style={{ padding: 15 }}
+            data={messages}
+            style={{ padding: 16 }}
             renderItem={renderItem}
+            keyExtractor={(item, index) => item.id + String(index)}
             inverted />
         </MessagesWrapper>
 
-        <ChatInput idChat={chat.idChat}/>
+        <ChatInput idChat={chat.idChat} />
       </Wrapper>
     </SafeAreaView>
   )
